c83c24cffd9b722d3c3fe47a2d43c08af6e031cc
umairaltaf982 <umairaltaf982@gmail.com>
2024-08-03 11:45:31 +0500

    loop changed

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ let display = document.querySelector('.input-box');
 let buttons = document.querySelectorAll('button');
 let buttonsArray = Array.from(buttons);
 let string = '';
-buttonsArray.forEach(function (butn) {
-    butn.addEventListener('click', function (event) {
-        if (event.target.innerHTML == 'CE') {
+for (let i = 0; i < buttonsArray.length; i++) {
+    buttonsArray[i].addEventListener('click', function (event) {
+        if (event.target.innerHTML == 'x') {
+            string += '*';
+            display.value = string;
+        }
+        else if (event.target.innerHTML == 'CE') {
             string = string.substring(0, string.length - 1);
             display.value = string;
         }
@@ -13,13 +17,14 @@ buttonsArray.forEach(function (butn) {
             display.value = string;
         }
         else if (event.target.innerHTML == '=') {
-            string = eval(string);
-            display.value = string;
+            let ans = eval(string);
+            console.log(string);
+            display.value = ans;
         }
         else {
             string += event.target.innerHTML;
             display.value = string;
         }
-
-    });
-});
+    }
+    );
+};
